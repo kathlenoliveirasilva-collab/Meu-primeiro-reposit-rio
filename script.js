5959b8dd917673f3c13a4ec4df541e8b1c88495a
kathlenoliveirasilva-collab <kathlen.oliveira.silva@escola.pr.gov.br>
2026-03-20 11:05:55 -0300

    script.js 3

--- script.js
+++ script.js
@@ -27,7 +27,7 @@ botaoCor.addEventListener("click", function () {
     if (corAtual == 0) {
 
         // se for 0, muda a cor de fundo do card para azul claro
-        card.style.backgroundColor = "#6ebdc6";
+        card.style.backgroundColor = "#05565f";
 
         // altera o valor da variável para 1, indicando que a cor foi trocada
         corAtual = 1;
